bbad4a853fd429b17c7f6d2d661f8ad866993d1b
Nico Barberis <nico09barberis@gmail.com>
2026-02-12 09:22:48 -0300

    correcion de ruta de importancion en una imagen

--- dyf-website/src/pages/servicesPage/DecorationPage.jsx
+++ dyf-website/src/pages/servicesPage/DecorationPage.jsx
@@ -17,7 +17,7 @@ export default function DecorationPage() {
       <HeroWithBreadcrumb
         title="Ambientación y decoración"
         breadcrumbItems={breadcrumb}
-        imageUrl="../../../public/images/home/slide4.webp"
+        imageUrl="/images/home/slide4.webp"
       />
 
       <DecorationHero />
